95bec1ecd563952f5c7f20dbe8cea0ca3a88b1b4
cssmagic <cssmagic.cn@gmail.com>
2022-05-24 01:09:47 +0800

    feat: 实现命令行参数解析、本地文件元数据解析的功能

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,42 @@
 #!/usr/bin/env node
 'use strict'
 
-console.log('OK')
+const yargs = require('yargs')
+const { main } = require('../lib/main')
+const { validate } = require('../lib/validate')
+
+const argv = yargs
+	.scriptName('kup')
+	.usage('Usage: $0 <file> [options]')
+	.example('kup foo.md --repo aaa/bbb --id 13', '// sync foo.md to GitHub issue aaa/bbb#13')
+	.option('repo', {
+		alias: 'r',
+		type: 'string',
+		description: 'Specify GitHub repository',
+	})
+	.option('id', {
+		alias: 'i',
+		type: 'number',
+		description: 'Specify GitHub issue ID',
+	})
+	.option('parse-only', {
+		alias: 'p',
+		type: 'boolean',
+		description: 'Only parse file, without posting or syncing',
+		hidden: true,
+	})
+	.alias('v', 'version')
+	.alias('h', 'help')
+	.argv
+
+// debug
+// console.log('[Kup] [Debug] argv =', argv)
+
+// validate args
+validate(argv)
+
+// do the job
+main(argv)
+	.then(() => {
+		console.log('[Kup] Done!')
+	})
